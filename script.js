14fda2d8a149f5ad676e1a72f8832b94fb90e0a3
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-19 21:25:22 +0000

    set up search function using input and option to select object propery

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+var data;
+
+const $request = $.ajax({url: "http://www.json-generator.com/api/json/get/bPZDYpeDiq?indent=2", success: function(result){
+    data = result; // assign results to variable
+  }});
+
 $('button').on('click', function () {
-	const patientName = $('#name').val()
-	$('#results').append(`<p>${patientName}</p>`)
-	
-})
+  var search = $('#search').val();
+  var searchType = $('select').val().toLowerCase();
+  // empty table for each search
+  $('#results').empty(); 
+	for (let i=0; i<data.length; i++) {
+    var person = data[i];
+    if (person[searchType].toLowerCase().includes(search)) { // searches for partial match
+      // append row to table
+      $('#results').append(`
+        <tr>
+          <td>${person.name}</td>
+          <td>${person.company}</td>
+          <td>${person.email}</td>
+          <td>${person.phone}</td>
+          <td>${person.balance}</td>
+          <td class="copy">Copy to clipboard</td>
+        </tr>`) 
+    }   
+  }
+}) 
+
+// Copy command
+
+$('#results').on('click', '.copy', function (event) { // need to select parent container, then use target class as parameter, since you are working with dynamic content
+
+  // var result = getElementsByTagName('tr')[event.target];
+  $query = $(event.target).parent()[0]
+  $query.select();
+  document.execCommand('copy')
+  
+}) 
+
+
+
