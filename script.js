ca76e01b4479230f7c170954779bbeb2e37ea6fb
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-20 21:33:18 +0000

    fixed error message bug

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
+// ==== Variables =====
+
 var data;
+
+// ==== AJAX call ====
 
 const $request = $.ajax({url: "http://www.json-generator.com/api/json/get/bPZDYpeDiq?indent=2", success: function(result){
     data = result; // assign results to variable
+    console.log(data)
   }});
 
-$('button').on('click', function () {
+// ==== Functions ====
+
+function showError () {
+  $('#error').css({'visibility':'visible'});
+}
+
+function hideError () {
+  $('#error').css({'visibility':'hidden'});
+}
+
+function searchRecords () {
   var search = $('#search').val();
-  var searchType = $('select').val().toLowerCase();
-  // empty table for each search
-  $('#results').empty(); 
-	for (let i=0; i<data.length; i++) {
-    var person = data[i];
-    if (person[searchType].toLowerCase().includes(search)) { // searches for partial match
-      // append row to table
-      $('#results').append(`
-        <tr>
-          <td>${person.name}</td>
-          <td>${person.company}</td>
-          <td>${person.email}</td>
-          <td>${person.phone}</td>
-          <td>${person.balance}</td>
-          <td class="copy">Copy to clipboard</td>
-        </tr>`) 
-    }   
+  if (search.length > 0) { // validate
+    var searchType = $('select').val().toLowerCase();
+    var match = false;
+    hideError();
+    // empty table for each search
+    $('#results').empty(); 
+    for (let i=0; i<data.length; i++) {
+      var person = data[i];
+      if (person[searchType].toLowerCase().includes(search)) { // searches for partial match
+        // append row to table
+        let personID = person.guid.substring(0,2);
+        $('#results').append(`
+          <tr>
+            <td>${personID}.</td>
+            <td>${person.name}</td>
+            <td>${person.company}</td>
+            <td>${person.address}</td>
+            <td>${person.email}</td>
+            <td>${person.phone}</td>
+            <td>${person.balance}</td>
+          </tr>`) 
+        match = true;
+      
+      } 
+        
+    if (match==false) {
+      showError();
+      $('#error').html('<p>No results. Please try again</p>');
+    }      
+      
+            
+    }
+  
+  } else {
+    showError();
+    $('#error').html('<p>Please enter a search term</p>'); // .html over append as you want to overwrite content, not add
   }
+} 
+
+// ==== Event Listeners ====
+
+
+$('button').on('click', function () {
+  searchRecords();  
 }) 
 
 // Copy command
